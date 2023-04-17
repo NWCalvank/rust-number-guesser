57459155d288495f3f5350accd037568fa8b43bb
Nathan Calvank <nwcalvank@gmail.com>
2023-04-16 18:59:20 -0700

    Options for guess range

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,27 @@ use rand::prelude::*;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let answer: u32 = thread_rng().gen_range(1..11);
+    let mut len = 10;
     let mut running = true;
+    let mut user_len = String::new();
     let mut user_auto = String::new();
 
-    // TODO: Generate this range
-    let mut guesses = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("Between 1 and ...? [(10)/100/1000]");
+    io::stdin().read_line(&mut user_len)?;
+
+    if user_len.trim() == "100" {
+        len = 100
+    }
+
+    if user_len.trim() == "1000" {
+        len = 1000
+    }
+
+    let mut guesses = vec![0; len];
+    for i in 1..len {
+        guesses[i] = i + 1
+    }
+    let answer = thread_rng().gen_range(1..len + 1);
 
     println!("Automate? [y/N]");
 
@@ -31,7 +46,7 @@ fn main() -> io::Result<()> {
             guesses.remove(index);
         }
     } else {
-        println!("Pick a number between 1 & 10 :)");
+        println!("Pick a number between 1 & {} :)", guesses.len());
 
         while running {
             let mut user_input = String::new();
@@ -45,6 +60,13 @@ fn main() -> io::Result<()> {
     }
 
     println!("Correct!");
+    let guess_count = len - guesses.len();
+
+    if guess_count > 1 {
+        println!("You took {} guesses.", guess_count)
+    } else {
+        println!("First try. You legend")
+    }
 
     Ok(())
 }
